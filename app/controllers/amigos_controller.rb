e8063de8abc898e472e5121653669f6449ae4852
HectorCataldo <akealonso@gmail.com>
2022-01-07 14:48:54 -0300

    Mejora de estilos Boostrap

--- app/controllers/amigos_controller.rb
+++ app/controllers/amigos_controller.rb
@@ -25,7 +25,7 @@ class AmigosController < ApplicationController
 
     respond_to do |format|
       if @amigo.save
-        format.html { redirect_to amigo_url(@amigo), notice: "Amigo was successfully created." }
+        format.html { redirect_to amigo_url(@amigo), notice: "Amigo ingresado exitosamente." }
         format.json { render :show, status: :created, location: @amigo }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class AmigosController < ApplicationController
   def update
     respond_to do |format|
       if @amigo.update(amigo_params)
-        format.html { redirect_to amigo_url(@amigo), notice: "Amigo was successfully updated." }
+        format.html { redirect_to amigo_url(@amigo), notice: "Amigo actualizado exitosamente." }
         format.json { render :show, status: :ok, location: @amigo }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class AmigosController < ApplicationController
     @amigo.destroy
 
     respond_to do |format|
-      format.html { redirect_to amigos_url, notice: "Amigo was successfully destroyed." }
+      format.html { redirect_to amigos_url, notice: "Amigo fue removido correctamente." }
       format.json { head :no_content }
     end
   end
